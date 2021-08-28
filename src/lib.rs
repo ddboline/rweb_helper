97f3b461ce4c31e40e3580af20440d40ffe5cd11
Daniel Boline <ddboline@gmail.com>
2021-08-28 14:16:52 -0400

    derive_rweb_schema

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,17 @@ pub mod response_description_trait;
 pub mod status_code_trait;
 
 pub use rweb_helper_macro::RwebResponse;
+
+#[macro_export]
+macro_rules! derive_rweb_schema {
+    ($T0:ty, $T1:ty) => {
+        impl rweb::openapi::Entity for $T0 {
+            fn type_name() -> std::borrow::Cow<'static, str> {
+                $T1::type_name()
+            }
+            fn describe(c: &mut rweb::openapi::ComponentDescriptor) -> rweb::openapi::ComponentOrInlineSchema {
+                $T1::describe(c)
+            }
+        }
+    }
+}
